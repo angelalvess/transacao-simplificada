1b8032acb8314ff590b79776b71581a0d3657590
angelalvess <angeltrashangel@gmail.com>
2025-04-28 19:33:05 -0300

    feat: criacao e implemetacao da Entity Carteira

---
+++ src/main/java/com/angel/transacao_simplificada/infrastructure/entities/Carteira.java
@@ -0,0 +1,26 @@
+package com.angel.transacao_simplificada.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "carteira")
+@Table
+@Builder
+public class Carteira {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal saldo;
+
+    @JoinColumn(name = "usuario_id")
+    @OneToOne
+    private Usuario usuario;
+}
