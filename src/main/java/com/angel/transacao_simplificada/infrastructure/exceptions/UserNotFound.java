b1005c05642d739eb7635066dca21f1e5c634791
angelalvess <angeltrashangel@gmail.com>
2025-05-05 10:54:28 -0300

    feat: criando e implementando Global Exception Handler

--- src/main/java/com/angel/transacao_simplificada/infrastructure/exceptions/UserNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.angel.transacao_simplificada.infrastructure.exceptions;
-
-public class UserNotFound extends RuntimeException {
-    public UserNotFound(String message) {
-        super(message);
-    }
-}
